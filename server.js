d0c57f07367343cd02dd6ae1d0dd41e22dc4ed5d
kayalTeeru <kayalu.teeru96@gmail.com>
2017-08-26 14:10:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,9 +7,26 @@ app.use(morgan('combined'));
 
 var articleOne =
 {
-  title:''  
+  title:' Article One | Kayalvizhi',
+  heading:'Article one',
+  date:'August 20,2017',
+  content:`<p>
+                 This is the content for my first article. This is the content for my first article.
+                 This is the content for my first article. This is the content for my first article. This is the content for myfirst article.
+             </p>
+             <p>
+                 This is the content for my first article. This is the content for my first article.
+                 This is the content for my first article. This is the content for my first article. This is the content for myfirst article.
+             </p>
+             <p>
+                 This is the content for my first article. This is the content for my first article.
+                 This is the content for my first article. This is the content for my first article. This is the content for myfirst article.`
 };
 
+function createTemplate(data)
+{
+    var htmlTemplate
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
